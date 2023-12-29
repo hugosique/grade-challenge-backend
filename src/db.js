0d2cac88e6f92ee75629ceb3f83e81288671ab4e
hugosique <otahugo02@gmail.com>
2023-12-28 21:50:12 -0300

    db name correction

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 connection.connect((error) => {
     if(error) throw error;
-    console.log(`Conectado ao banco de dados: ${process.env.DB_NAME}`)
+    console.log(`Conectado ao banco de dados`)
 });
 
 module.exports = connection;
